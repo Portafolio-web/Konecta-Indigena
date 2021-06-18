d958302a24926acf9e6f8a820b0e5d05c4ae5cb0
Portafolio-web <78131157+Portafolio-web@users.noreply.github.com>
2021-06-17 20:04:35 -0500

    Add files via upload

--- contact.php
+++ contact.php
@@ -1,21 +1,13 @@
 <?php
-
 	if (isset($_POST["submit"])) {
-
 		$name = $_POST['name'];
 		$email = $_POST['email'];
 		$message = $_POST['message'];
 		$from = 'Coordial saludo, este mensaje viene de parte de Konecta Indígena'; 
-		$to = 'kevinstikbernalbaccapro@gmail.com'; 
+		$to = 'kevinstikbernalbaccapro@gmail.com';
 		$subject = $_POST['subject']; 
-		
 		$body = "From: $name\n E-Mail: $email\n Subject: $subject\n Message:\n $message";
-	
-
 	mail($to, $subject, $body, $from) or die("Error!");
-
 	header("location: gracias.html");
-	
 	}
-	
 ?>
